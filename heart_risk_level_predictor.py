71daae7078252e95a4afb3302aec1aba1046a93b
Geethmila Jayasooriya <geethmilajayasooriya2001@gmail.com>
2026-01-10 15:46:28 +0530

    Update heart_risk_level_predictor.py

--- heart_risk_level_predictor.py
+++ heart_risk_level_predictor.py
@@ -3,15 +3,6 @@ import joblib
 import numpy as np
 
 model=joblib.load('heart_risk_prediction_regression_model.sav')
-#The machine learning model was already trained earlier
-
-#It is saved as a .sav file
-
-#joblib.load() loads it into memory
-
-#👉 Interview line:
-
-#“We load a pre-trained regression model using Joblib to make predictions.”
 
 app=Flask(__name__) #application
 
@@ -42,23 +33,14 @@ def getresults():
 	bpm=float(result['bpm'])
 	diab=float(result['diab'])
 
-	#Why float()?
-
-#ML models only understand numbers
-
-#HTML sends values as strings
-
-#👉 Interview line:
-
-#“Form inputs are converted into numerical values for model compatibility.”
 
 	test_data=np.array([gender,age,tc,hdl,smoke,bpm,diab]).reshape(1,-1)
-	#“The input data is reshaped into a 2D array before prediction.”
+	
 
 	prediction=model.predict(test_data)
-	#The model calculates the heart disease risk
+	# calculates  heart disease risk
 
-    #Output is usually an array like [[23.5678]]
+   
 
 	resultDict={"name":name,"risk":round(prediction[0][0],2)}
 	#Rounds risk to 2 decimal places
@@ -69,14 +51,3 @@ def getresults():
 
 app.run(debug=True)
 
-#Starts Flask server
-
-#debug=True:
-
-#Shows errors
-
-#Auto-reloads code changes
-
-#👉 Interview line:
-
-#“Debug mode helps during development.”
